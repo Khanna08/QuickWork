25813413a7e4289c1b3c67688e1199cfd4d0f73e
Khanna08 <akshitkhanna69@gmail.com>
2020-10-31 21:21:54 +0530

    Update createMail.js

--- createMail.js
+++ createMail.js
@@ -1,23 +1,33 @@
-const {google} = require('googleapis');
+const {google} = require('googleapis'); // google rest API
 const mailComposer = require('nodemailer/lib/mail-composer'); // nodemailer library
-	
+
+// custom class for executing mail options
 class CreateMail{
 
-	constructor(auth, me, to, sub, body, task){
-		this.me = me;
+	/**
+	 auth is used for oAuth 2.0 client
+	 to stores email address of recipient
+	 sub stors the subject
+	 body is used to store text body of the mail(only text option in this app)
+	 if value of task is 'send' then mail is sent otherwise it is saved as draft
+	**/
+	constructor(auth, to, sub, body, task){
 		this.task = task;
 		this.auth = auth;
 		this.to = to;
 		this.sub = sub;
 		this.body = body;
+
+		// this stores the current user gmail credentials
 		this.gmail = google.gmail({version: 'v1', auth});
-		console.log(this.gmail);
+		// console.log(this.gmail);
 	}
 
 	//Creates the mail body and encodes it to base64 format.
 	makeBody(){
+
+		// an object for mail
 		let mail;
-		let status;
 		mail = new mailComposer({
 			to: this.to,
 			text: this.body,
@@ -35,8 +45,8 @@ class CreateMail{
 			  .replace(/\//g, '_')
 			  .replace(/=+$/, '');
 			
+			// task is user specified
 			if(this.task === 'send'){
-				console.log('sending data');
 				this.sendMail(encodedMessage);
 			}
 			this.saveDraft(encodedMessage);
@@ -46,7 +56,7 @@ class CreateMail{
 	//Send the message to specified receiver.
 	sendMail(encodedMessage){
 		this.gmail.users.messages.send({
-			userId: this.me,
+			userId: "me",
 			resource: {
 				raw: encodedMessage,
 			}
@@ -72,4 +82,5 @@ class CreateMail{
 	}
 
 }
+// making createmail available for the repository
 module.exports = CreateMail;
